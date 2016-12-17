7699add8d57a93a33067c8572c22db3bf158c345
小胖珊 <@YiShan>
2016-12-18 01:53:50 +0800

    1

---
+++ src/fcu/iecs/oop/pokemon/Pokemon.java
@@ -0,0 +1,41 @@
+package fcu.iecs.oop.pokemon;
+
+
+
+public abstract class Pokemon {
+	
+	public enum PokemonType {
+		  FIRE, WATER, GRASS
+		}
+
+	
+	public final String name = null;
+	public final PokemonType type = null;
+	public int cp ;
+	public abstract void attack();
+	
+	
+	public int getCp() {
+		return cp;
+	}
+	public void setCp(int cp) {
+		this.cp = cp;
+	}
+	public String getName() {
+		return name;
+	}
+	public PokemonType getType() {
+		return type;
+	}
+	
+	public Pokemon(String name ,PokemonType type, int cp)
+	{
+		name = this.name;
+		type = this.type;
+		cp = this.cp;
+	}
+	
+	
+
+
+}
